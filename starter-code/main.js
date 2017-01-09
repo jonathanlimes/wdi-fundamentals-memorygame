df53e4b8d8859a512c39ca5edd1ab9bdfefc2f19
Jonathan Lim <jonathanlimes@gmail.com>
2017-01-09 22:50:09 +0800

    added if/else game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,11 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert("Sorry, try again.");
+} else if (cardTwo === cardThree) {
+	alert("Sorry, try again.");
+} else {
+	alert("You found a match!");
+}
