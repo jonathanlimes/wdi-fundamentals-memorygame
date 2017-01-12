211841792601ff5d4ed2078c6a52fed5e57bdfa3
Jonathan Lim <jonathanlimes@gmail.com>
2017-01-12 18:12:57 +0800

    completed Unit 10

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,10 +5,22 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour) {
-	alert("Sorry, try again.");
-} else if (cardTwo === cardThree) {
-	alert("Sorry, try again.");
-} else {
-	alert("You found a match!");
+// if (cardTwo === cardFour) {
+// 	alert("Sorry, try again.");
+// } else if (cardTwo === cardThree) {
+// 	alert("Sorry, try again.");
+// } else {
+// 	alert("You found a match!");
+// }
+
+var createCards = function () {
+	var getGame = document.getElementById('game-board');
+	for (var i = 0; i < 4; i++) {
+		var createDiv = document.createElement('div');
+		createDiv.className = 'card';
+		getGame.appendChild(createDiv);
+	}
+	document.getElementsByClassName('card').setAttribute('id', 'game-board');
 }
+
+createCards();
